82cca445b644923d561d57e3da49a70bd2bc974e
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-01-04 21:43:07 +0530

    Add svg to the html

--- first-chart/chart.js
+++ first-chart/chart.js
@@ -1,5 +1,38 @@
 async function drawLineChart() {
   const dataset = await d3.json("./../my_weather_data.json");
+
+  const yAccessor = (d) => d.temperatureMax;
+
+  const dateParser = d3.timeParse("%Y-%m-%d");
+  const xAccessor = (d) => dateParser(d.date);
+
+  let dimensions = {
+    width: window.innerWidth * 0.9,
+    height: 400,
+    margin: {
+      top: 15,
+      right: 15,
+      bottom: 40,
+      left: 60,
+    },
+  };
+  dimensions.boundWidth =
+    dimensions.width - dimensions.margin.left - dimensions.margin.right;
+  dimensions.boundHeight =
+    dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
+
+  const wrapper = d3
+    .select("#wrapper")
+    .append("svg")
+    .attr("width", dimensions.width)
+    .attr("height", dimensions.height);
+
+  const bounds = wrapper
+    .append("g")
+    .style(
+      "transform",
+      `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`
+    );
 }
 
 drawLineChart();
